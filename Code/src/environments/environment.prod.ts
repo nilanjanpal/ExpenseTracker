18fd5509b16efa2e2ecdbb33ce449028a5203ce4
Nilanjan Pal <nilanjan.pal06@gmail.com>
2023-06-26 22:59:01 +0530

    api url corrected

--- Code/src/environments/environment.prod.ts
+++ Code/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: "https://expense-tracker-backend-production-4d1b.up.railway.app/api/"
-  // url: "http://localhost:8080/api/"
+  // url: "https://expense-tracker-backend-production-4d1b.up.railway.app/api/"
+  url: "http://localhost:8080/api/"
 };
